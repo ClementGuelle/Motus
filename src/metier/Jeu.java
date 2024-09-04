0406648c61426ce0f45b2438eba3dad7e959aebb
betonfixe <clement.guelle@bbox.fr>
2024-09-04 12:16:41 +0200

    correction erreur avec les M(auvais placement)

--- src/metier/Jeu.java
+++ src/metier/Jeu.java
@@ -99,9 +99,10 @@ public class Jeu
 	public char[] VerifEmplacementCaratere(String motJoueur)
 	{
 
-		char[]  placementCaractere = new char[this.motAChercher.length()];
-		boolean caractereExiste    = false;
-		int     nbFoisCaratere     = 0;
+		char[]  placementCaractere    = new char[this.motAChercher.length()];
+		boolean caractereExiste       = false;
+		int     nbFoisCaratere        = 0;
+		int     nbFoisCaractereValide = 0;
 
 		for (int i = 0; i < placementCaractere.length; i++)
 		{
@@ -139,11 +140,48 @@ public class Jeu
 
 					if ( nbFoisCaratere > 0 )
 						placementCaractere[i] = 'M';
+					else
+						placementCaractere[i] = 'I';
+
+					caractereExiste = false;
+					nbFoisCaratere  = 0;
+
 				}
 			}
 			
+		}
 
+		// Apres avoir fini le tour de tous les caracteres dans le mot il y a des erreurs avec les M alors que toutes les lettres peuvent être bien placé
+		for (int i = 0; i < placementCaractere.length; i++)
+		{
+			if (placementCaractere[i] == 'M')
+			{
 
+				// Permet de calculer le nombre de fois où la lettre est présente dans le mot
+				for (int j = 0; j < placementCaractere.length; j++)
+				{
+					if ( motJoueur.charAt(j) == motJoueur.charAt(i) )
+						nbFoisCaratere++;
+				}
+
+				System.out.println(" nb " + motJoueur.charAt(i) + " dans le mot : " + nbFoisCaractereValide);
+
+				// Permet de calculer le nombre de fois où la lettre est bien placé
+				for (int j = 0; j < placementCaractere.length; j++)
+				{
+					if ( motJoueur.charAt(j) == motJoueur.charAt(i) && placementCaractere[j] == 'B')
+						nbFoisCaractereValide ++;
+				}
+
+				System.out.println(" nb caractere bon : " + nbFoisCaractereValide);
+
+				// Soustrait le nombre de fois que le caractere est bien placé avec le nombre de fois que le caractere existe comme ca cela permet de savoir si il est M ou I
+				if ( nbFoisCaratere > nbFoisCaractereValide )
+					placementCaractere[i] = 'I';
+
+				nbFoisCaratere        = 0;
+				nbFoisCaractereValide = 0;
+			}
 		}
 
 		for (int i = 0; i < placementCaractere.length; i++) 
